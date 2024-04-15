d7362d52b434a30324a0d898fecf26f1ef202743
christophemr <christophemr59@gmail.com>
2024-04-15 18:33:39 +0200

    method to handle @app.teardown_appcontext added

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,8 +10,15 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def close_db(obj):
+    """ calls methods close() """
+    storage.close()
+
 
 if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
+
+    HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', default=5000))
+
     app.run(host=HOST, port=PORT, threaded=True)
