4afaebc68aa61f34b03e9cd48eb2224bb4b631aa
christophemr <christophemr59@gmail.com>
2024-04-15 19:17:21 +0200

    strict_slashes added in route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from flask import Flask
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def api_status():
     """
     Retrieves the status of the API.
