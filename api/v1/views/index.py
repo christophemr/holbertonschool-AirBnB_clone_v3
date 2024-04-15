7b044f38d3fb877340eaf7729fe647b1dbbf292f
christophemr <christophemr59@gmail.com>
2024-04-15 21:14:46 +0200

    count() method from storage added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """
 creat a Flask app; app_views
 """
+from models import storage
 from flask import jsonify
 from flask import Flask
 from api.v1.views import app_views
@@ -15,3 +16,16 @@ def api_status():
         JSON response indicating the status of the API
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
