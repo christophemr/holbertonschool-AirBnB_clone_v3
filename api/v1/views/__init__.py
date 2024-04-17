45f6f3ae64851abf5d81a443ede09b9cd6f9c41d
christophemr <christophemr59@gmail.com>
2024-04-17 08:35:37 +0200

    entire refactoring

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,31 @@
-#!usr/bin/python3
-"""
-create a blueprint for the api
-"""
+#!/usr/bin/python3
+"""Script that creates a blueprint for the API"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-import api.v1.views.index
-import api.v1.views.states
-import api.v1.views.cities
-import api.v1.views.amenities
-import api.v1.views.users
-import api.v1.views.places
+def register_views():
+    """Method that registers the views"""
+    import api.v1.views.index
+    import api.v1.views.states
+    import api.v1.views.cities
+    import api.v1.views.amenities
+    import api.v1.views.users
+    import api.v1.views.places
+
+
+register_views()
+
+"""
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+"""
