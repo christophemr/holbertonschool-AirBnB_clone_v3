11afb6e09d8821d1d1f31bdf4b525f9d52db3bec
christophemr <christophemr59@gmail.com>
2024-04-17 08:57:14 +0200

    hotfix

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,8 @@ This module contains the principal application
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
 from os import getenv
 from flask_cors import CORS
 
