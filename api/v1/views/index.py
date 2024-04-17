55e8b5ca7851d4fec4ed875de531161b225b840c
christophemr <christophemr59@gmail.com>
2024-04-17 08:56:43 +0200

    hotfix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,13 +5,6 @@ Create a Flask app; app_views
 from flask import jsonify
 from flask import Flask
 from api.v1.views import app_views
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -31,6 +24,13 @@ def get_object_counts():
     Returns:
         JSON response with counts for different object types
     """
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
     counts = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
