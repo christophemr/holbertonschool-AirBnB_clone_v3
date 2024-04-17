f805fcd5703ce69d177743673a7109985d6d2ba2
christophemr <christophemr59@gmail.com>
2024-04-17 14:59:00 +0200

    hotfix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,32 +5,33 @@ Create a Flask app; app_views
 from flask import jsonify
 from flask import Flask
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
+
+@app_views.route('/status')
 def api_status():
     """
-    Retrieves the status of the API.
     Returns:
         JSON response indicating the status of the API
     """
-    return jsonify({"status": "OK"})
+    response = {"status": "OK"}
+    return jsonify(response)
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats')
 def get_object_counts():
     """
     Retrieves the number of each object by type.
     Returns:
         JSON response with counts for different object types
     """
-    from models import storage
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
     counts = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
