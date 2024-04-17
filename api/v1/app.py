69e6aa542a9dc9fb39ba5b30e69435834c921dc2
christophemr <christophemr59@gmail.com>
2024-04-17 14:59:40 +0200

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,7 +16,7 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(obj):
+def close_db(exception):
     """ Calls methods close() """
     storage.close()
 
@@ -24,10 +24,11 @@ def close_db(obj):
 @app.errorhandler(404)
 def page_not_found(error):
     """ Loads a custom 404 page not found """
-    return jsonify({"error": "Not found"}), 404
+    response = {"error": "Not found"}
+    return jsonify(response), 404
 
 
 if __name__ == "__main__":
     API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    API_PORT = getenv('HBNB_API_PORT', 5000)
+    API_PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=API_HOST, port=API_PORT, threaded=True)
