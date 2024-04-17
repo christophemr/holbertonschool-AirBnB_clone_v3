107215c47d941229830c224e1a224cffdffd70d5
christophemr <christophemr59@gmail.com>
2024-04-17 08:22:51 +0200

    classes import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,12 @@ from models import storage
 from flask import jsonify
 from flask import Flask
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
